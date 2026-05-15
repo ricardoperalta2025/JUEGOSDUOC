cc75c5940e9bf065a3dbd96039aa5dcaf9a13394
ricardoperalta2025 <ri.peralta@duocuc.cl>
2026-05-14 21:31:18 -0400

    Creación Proyecto

---
+++ src/test/java/com/DUOC/JUEGOS/JuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DUOC.JUEGOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
