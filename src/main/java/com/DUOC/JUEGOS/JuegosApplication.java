d78a0ef49adea5847429ffa7ab920ec25d0758a0
ricardoperalta2025 <ri.peralta@duocuc.cl>
2026-06-25 05:06:55 -0400

    Eureka y Api-Gateway

--- src/main/java/com/DUOC/JUEGOS/JuegosApplication.java
+++ src/main/java/com/DUOC/JUEGOS/JuegosApplication.java
@@ -2,16 +2,17 @@ package com.DUOC.JUEGOS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient 
 public class JuegosApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JuegosApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JuegosApplication.class, args);
+    }
 }
 
 // para copiar y crear la bd en laragon y testear
 // db_videojuegos_vm
+// Bd para los test: db_videojuegos_dev y db_videojuegos_test
 // http://localhost:8080/doc/swagger-ui.html
